b04b2e994d5f0eecdb72ce45f6b5c1fa42b5636a
hiepnxph <hiepnxph@gmail.com>
2022-07-11 13:55:17 +0700

    Add project files.

---
+++ FPTS/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace FPTS.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin",
+                "{controller}/{action}/{id}",
+                new {action = "Index", controller = "Admin", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
